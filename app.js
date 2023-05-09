ef5a9781fc114009964fe997dc744cebfb46afbf
hinacarlo <hinacarlo1993@gmail.com>
2023-05-10 00:49:09 +0800

    Setup express and connection to mongodb

--- app.js
+++ app.js
@@ -1,1 +1,21 @@
-console.log('E-Commerce API');
+const express = require('express')
+const connectDB = require('./db/connect')
+
+const app = express()
+
+require('dotenv').config()
+
+
+const PORT = process.env.PORT || 5000
+
+const start = async () => {
+    try {
+        await connectDB(process.env.MONGO_URL)
+        app.listen(PORT, () => console.log(`Server is listening on port ${PORT}...`))
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+start()
+
--- db/connect.js
+++ db/connect.js
@@ -1,7 +1,13 @@
 const mongoose = require('mongoose');
 
 const connectDB = (url) => {
-  return mongoose.connect(url);
-};
+    return mongoose.connect(url)
+}
+
+mongoose.connection.on('error', error => {
+    console.log(error)
+}) 
+
+mongoose.connection.on('disconnected', () => console.log('Server disconnected from MongoDB'))
 
 module.exports = connectDB;
