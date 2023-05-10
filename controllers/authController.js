c5541f413f2cdfff887d341299b03e1dbb670d81
hinacarlo <hinacarlo1993@gmail.com>
2023-05-10 21:44:05 +0800

    Implement login functionality with cookies

--- controllers/authController.js
+++ controllers/authController.js
@@ -22,13 +22,35 @@ const registerController = async (req, res) => {
     const user = await User.create({ name, email, password, role })
 
     const tokenUser = { name: user.name, userId: user._id, role: user.role }
-    attachCookiesToResponse({res, user: tokenUser})
+    attachCookiesToResponse({ res, user: tokenUser })
 
     res.status(StatusCodes.CREATED).json({ user: tokenUser })
 
 }
 const loginController = async (req, res) => {
-    res.send('login user')
+    const { email, password } = req.body
+
+    if (!email || !password) {
+        throw new BadRequestError('Please provide email and password')
+    }
+
+    const user = await User.findOne({ email })
+
+    if (!user) {
+        throw new UnauthenticatedError('Invalid Credentials')
+    }
+
+    const isPasswordCorrect = await user.comparePassword(password)
+
+    if (!isPasswordCorrect) {
+        throw new UnauthenticatedError('Invalid Credentials')
+    }
+
+    const tokenUser = { name: user.name, userId: user._id, role: user.role }
+    attachCookiesToResponse({ res, user: tokenUser })
+
+    res.status(StatusCodes.CREATED).json({ user: tokenUser })
+
 }
 const logoutController = async (req, res) => {
     res.send('logout user')
